c3a147f82bd429fbde0ccf33569bc73e9044148d
Ashwith reddy <137775785+ashwith123@users.noreply.github.com>
2024-03-20 22:14:29 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,34 @@
+let btn=document.querySelector("button");
+let inp=document.querySelector("input");
+let ul=document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+
+    let clist=document.createElement("li");
+    clist.innerText=inp.value;
+
+    let del=document.createElement("button");
+    del.innerText="delete";
+    del.classList.add("delete")
+    clist.appendChild(del);
+
+    let done=document.createElement("button");
+    done.classList.add("done")
+    done.innerText="done";
+    clist.appendChild(done);
+
+    ul.appendChild(clist);
+    inp.value="";
+})
+
+ul.addEventListener("click",function(event){
+    if(event.target.classList.contains("delete")){
+        event.target.parentNode.remove();
+    }
+    if(event.target.classList.contains("done")){
+        let parentelement=event.target.parentNode;
+        parentelement.classList.add("strike");
+    }
+
+
+})
