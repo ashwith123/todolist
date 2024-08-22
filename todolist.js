f0315e06afc113b71dc30c746b8c5e61b96d122b
ashwith <ashwith9492@gmail.com>
2024-08-22 22:09:54 +0530

    made visually apealing

--- todolist.js
+++ todolist.js
@@ -1,34 +1,31 @@
-let btn=document.querySelector("button");
-let inp=document.querySelector("input");
-let ul=document.querySelector("ul");
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function () {
+  let clist = document.createElement("li");
+  clist.innerHTML = "<strong>" + inp.value + "</strong>";
 
-    let clist=document.createElement("li");
-    clist.innerText=inp.value;
+  let del = document.createElement("button");
+  del.innerText = "delete";
+  del.classList.add("delete");
+  clist.appendChild(del);
 
-    let del=document.createElement("button");
-    del.innerText="delete";
-    del.classList.add("delete")
-    clist.appendChild(del);
+  let done = document.createElement("button");
+  done.classList.add("done");
+  done.innerText = "done";
+  clist.appendChild(done);
 
-    let done=document.createElement("button");
-    done.classList.add("done")
-    done.innerText="done";
-    clist.appendChild(done);
+  ul.appendChild(clist);
+  inp.value = "";
+});
 
-    ul.appendChild(clist);
-    inp.value="";
-})
-
-ul.addEventListener("click",function(event){
-    if(event.target.classList.contains("delete")){
-        event.target.parentNode.remove();
-    }
-    if(event.target.classList.contains("done")){
-        let parentelement=event.target.parentNode;
-        parentelement.classList.add("strike");
-    }
-
-
-})
+ul.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete")) {
+    event.target.parentNode.remove();
+  }
+  if (event.target.classList.contains("done")) {
+    let parentelement = event.target.parentNode;
+    parentelement.classList.add("strike");
+  }
+});
